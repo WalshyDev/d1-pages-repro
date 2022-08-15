38e2db36b5b06e9f41c8aae95381b59b36f8bbea
Daniel Walsh <walshy@cloudflare.com>
2022-08-15 13:31:02 +0100

    async

--- functions/d1.js
+++ functions/d1.js
@@ -1,4 +1,4 @@
-export function onRequest({ env }) {
+export async function onRequest({ env }) {
 	try {
 		await env.DB.exec('CREATE TABLE IF NOT EXISTS `test` (`uuid` TEXT PRIMARY KEY)');
 		await env.DB.exec("INSERT INTO `test` (`uuid`) VALUES ('" + crypto.randomUUID() + "')");
