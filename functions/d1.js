d19a2d9ca787bbc7432874019d5f46dc9f11703a
Daniel Walsh <walshy@cloudflare.com>
2022-08-15 13:58:43 +0100

    Does this work?

--- functions/d1.js
+++ functions/d1.js
@@ -1,9 +1,13 @@
+import { Database } from '@cloudflare/d1';
+
 export async function onRequest({ env }) {
 	try {
-		await env.DB.exec('CREATE TABLE IF NOT EXISTS `test` (`uuid` TEXT PRIMARY KEY)');
-		await env.DB.exec("INSERT INTO `test` (`uuid`) VALUES ('" + crypto.randomUUID() + "')");
+		const DB = new Database(env.DB);
 
-		const ps = env.DB.prepare('SELECT * FROM `test`');
+		await DB.exec('CREATE TABLE IF NOT EXISTS `test` (`uuid` TEXT PRIMARY KEY)');
+		await DB.exec("INSERT INTO `test` (`uuid`) VALUES ('" + crypto.randomUUID() + "')");
+
+		const ps = DB.prepare('SELECT * FROM `test`');
 		const res = await ps.all();
 
 		return Response.json(res.results);
