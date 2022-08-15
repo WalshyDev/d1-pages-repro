e61a50c849066293562af6fdcfa6bedb65873c7c
Daniel Walsh <walshy@cloudflare.com>
2022-08-15 13:30:16 +0100

    push repro

---
+++ functions/d1.js
@@ -0,0 +1,13 @@
+export function onRequest({ env }) {
+	try {
+		await env.DB.exec('CREATE TABLE IF NOT EXISTS `test` (`uuid` TEXT PRIMARY KEY)');
+		await env.DB.exec("INSERT INTO `test` (`uuid`) VALUES ('" + crypto.randomUUID() + "')");
+
+		const ps = env.DB.prepare('SELECT * FROM `test`');
+		const res = await ps.all();
+
+		return Response.json(res.results);
+	} catch(e) {
+		return Response.json({ message: e.message, stack: e.stack, d1: env.DB });
+	}
+}
